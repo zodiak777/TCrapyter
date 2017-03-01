3f0b4c05211c2106b7efa4215d043f06f334a36f
zodiak777 <sebastian087@hotmail.de>
2017-03-01 17:45:08 +0100

    Add files via upload

---
+++ TCrypter/TCrypter/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace TCrypter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.keyBox = new System.Windows.Forms.TextBox();
+            this.inputBox = new System.Windows.Forms.RichTextBox();
+            this.outputBox = new System.Windows.Forms.RichTextBox();
+            this.load_txt = new System.Windows.Forms.PictureBox();
+            this.save = new System.Windows.Forms.PictureBox();
+            this.encryption = new System.Windows.Forms.PictureBox();
+            this.decryption = new System.Windows.Forms.PictureBox();
+            this.close = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.load_txt)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.save)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.encryption)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.decryption)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.close)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // keyBox
+            // 
+            this.keyBox.Location = new System.Drawing.Point(12, 416);
+            this.keyBox.Name = "keyBox";
+            this.keyBox.Size = new System.Drawing.Size(776, 20);
+            this.keyBox.TabIndex = 0;
+            this.keyBox.Text = "Key";
+            // 
+            // inputBox
+            // 
+            this.inputBox.Location = new System.Drawing.Point(12, 12);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(385, 398);
+            this.inputBox.TabIndex = 9;
+            this.inputBox.Text = "Input";
+            // 
+            // outputBox
+            // 
+            this.outputBox.Location = new System.Drawing.Point(403, 12);
+            this.outputBox.Name = "outputBox";
+            this.outputBox.Size = new System.Drawing.Size(385, 398);
+            this.outputBox.TabIndex = 4;
+            this.outputBox.Text = "Output";
+            // 
+            // load_txt
+            // 
+            this.load_txt.BackColor = System.Drawing.Color.Transparent;
+            this.load_txt.Image = ((System.Drawing.Image)(resources.GetObject("load_txt.Image")));
+            this.load_txt.Location = new System.Drawing.Point(12, 442);
+            this.load_txt.Name = "load_txt";
+            this.load_txt.Size = new System.Drawing.Size(119, 26);
+            this.load_txt.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.load_txt.TabIndex = 10;
+            this.load_txt.TabStop = false;
+            this.load_txt.Click += new System.EventHandler(this.load_txt_Click);
+            this.load_txt.MouseLeave += new System.EventHandler(this.load_txt_MouseLeave);
+            this.load_txt.MouseHover += new System.EventHandler(this.load_txt_MouseHover);
+            // 
+            // save
+            // 
+            this.save.BackColor = System.Drawing.Color.Transparent;
+            this.save.Image = ((System.Drawing.Image)(resources.GetObject("save.Image")));
+            this.save.Location = new System.Drawing.Point(137, 442);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(119, 26);
+            this.save.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.save.TabIndex = 10;
+            this.save.TabStop = false;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            this.save.MouseLeave += new System.EventHandler(this.save_MouseLeave);
+            this.save.MouseHover += new System.EventHandler(this.save_MouseHover);
+            // 
+            // encryption
+            // 
+            this.encryption.BackColor = System.Drawing.Color.Transparent;
+            this.encryption.Image = ((System.Drawing.Image)(resources.GetObject("encryption.Image")));
+            this.encryption.Location = new System.Drawing.Point(337, 442);
+            this.encryption.Name = "encryption";
+            this.encryption.Size = new System.Drawing.Size(119, 26);
+            this.encryption.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.encryption.TabIndex = 10;
+            this.encryption.TabStop = false;
+            this.encryption.Click += new System.EventHandler(this.encryption_Click);
+            this.encryption.MouseLeave += new System.EventHandler(this.encryption_MouseLeave);
+            this.encryption.MouseHover += new System.EventHandler(this.encryption_MouseHover);
+            // 
+            // decryption
+            // 
+            this.decryption.BackColor = System.Drawing.Color.Transparent;
+            this.decryption.Image = ((System.Drawing.Image)(resources.GetObject("decryption.Image")));
+            this.decryption.Location = new System.Drawing.Point(462, 442);
+            this.decryption.Name = "decryption";
+            this.decryption.Size = new System.Drawing.Size(119, 26);
+            this.decryption.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.decryption.TabIndex = 10;
+            this.decryption.TabStop = false;
+            this.decryption.Click += new System.EventHandler(this.decryption_Click);
+            this.decryption.MouseLeave += new System.EventHandler(this.decryption_MouseLeave);
+            this.decryption.MouseHover += new System.EventHandler(this.decryption_MouseHover);
+            // 
+            // close
+            // 
+            this.close.BackColor = System.Drawing.Color.Transparent;
+            this.close.Image = ((System.Drawing.Image)(resources.GetObject("close.Image")));
+            this.close.Location = new System.Drawing.Point(669, 442);
+            this.close.Name = "close";
+            this.close.Size = new System.Drawing.Size(119, 26);
+            this.close.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.close.TabIndex = 10;
+            this.close.TabStop = false;
+            this.close.Click += new System.EventHandler(this.close_Click);
+            this.close.MouseLeave += new System.EventHandler(this.close_MouseLeave);
+            this.close.MouseHover += new System.EventHandler(this.close_MouseHover);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 480);
+            this.Controls.Add(this.close);
+            this.Controls.Add(this.decryption);
+            this.Controls.Add(this.encryption);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.load_txt);
+            this.Controls.Add(this.outputBox);
+            this.Controls.Add(this.inputBox);
+            this.Controls.Add(this.keyBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "TCrypter";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            ((System.ComponentModel.ISupportInitialize)(this.load_txt)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.save)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.encryption)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.decryption)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.close)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        public System.Windows.Forms.TextBox keyBox;
+        public System.Windows.Forms.RichTextBox inputBox;
+        public System.Windows.Forms.RichTextBox outputBox;
+        private System.Windows.Forms.PictureBox load_txt;
+        private System.Windows.Forms.PictureBox save;
+        private System.Windows.Forms.PictureBox encryption;
+        private System.Windows.Forms.PictureBox decryption;
+        private System.Windows.Forms.PictureBox close;
+    }
+}
+
---
+++ TCrypter/TCrypter/Form1.cs
@@ -0,0 +1,193 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+using System.IO.Compression;
+using TCrypter.Properties;
+using System.Runtime.InteropServices;
+
+namespace TCrypter
+{
+    public partial class Form1 : Form
+    {
+        static string PasswordHash = "";
+        static string SaltKey = "adf30f92fjfpw2adf30f92fjfpw2f39a";
+        static string VIKey = "adf30f92fjfpw2ad";
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            inputBox.Text = "Input";
+            outputBox.Text = "Output";
+            keyBox.Text = "Encryption Key";
+        }
+
+        //Simple Methode zum bewegen des Fensters ohne den Rahmen
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        public static string Encrypt(string plainText)
+        {
+            byte[] plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+
+            byte[] keyBytes = new Rfc2898DeriveBytes(PasswordHash, Encoding.ASCII.GetBytes(SaltKey)).GetBytes(256 / 8);
+            var symmetricKey = new RijndaelManaged() { Mode = CipherMode.CBC, Padding = PaddingMode.Zeros };
+            var encryptor = symmetricKey.CreateEncryptor(keyBytes, Encoding.ASCII.GetBytes(VIKey));
+
+            byte[] cipherTextBytes;
+
+            using (var memoryStream = new MemoryStream())
+            {
+                using (var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
+                {
+                    cryptoStream.Write(plainTextBytes, 0, plainTextBytes.Length);
+                    cryptoStream.FlushFinalBlock();
+                    cipherTextBytes = memoryStream.ToArray();
+                    cryptoStream.Close();
+                }
+                memoryStream.Close();
+            }
+            return Convert.ToBase64String(cipherTextBytes);
+        }
+
+        public static string Decrypt(string encryptedText)
+        {
+            byte[] cipherTextBytes = Convert.FromBase64String(encryptedText);
+            byte[] keyBytes = new Rfc2898DeriveBytes(PasswordHash, Encoding.ASCII.GetBytes(SaltKey)).GetBytes(256 / 8);
+            var symmetricKey = new RijndaelManaged() { Mode = CipherMode.CBC, Padding = PaddingMode.None };
+
+            var decryptor = symmetricKey.CreateDecryptor(keyBytes, Encoding.ASCII.GetBytes(VIKey));
+            var memoryStream = new MemoryStream(cipherTextBytes);
+            var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read);
+            byte[] plainTextBytes = new byte[cipherTextBytes.Length];
+
+            int decryptedByteCount = cryptoStream.Read(plainTextBytes, 0, plainTextBytes.Length);
+            memoryStream.Close();
+            cryptoStream.Close();
+            return Encoding.UTF8.GetString(plainTextBytes, 0, decryptedByteCount).TrimEnd("\0".ToCharArray());
+        }
+
+        private void load_txt_MouseHover(object sender, EventArgs e)
+        {
+            load_txt.Image = Resources.load_a;
+        }
+
+        private void load_txt_MouseLeave(object sender, EventArgs e)
+        {
+            load_txt.Image = Resources.load_b;
+        }
+
+        private void load_txt_Click(object sender, EventArgs e)
+        {
+            Stream txtStream;
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if ((txtStream = openFileDialog.OpenFile()) != null)
+                {
+                    String strfile = openFileDialog.FileName;
+                    inputBox.Text = File.ReadAllText(strfile, Encoding.Default);
+                }
+            }
+        }
+
+        private void close_MouseHover(object sender, EventArgs e)
+        {
+            close.Image = Resources.close_a;
+        }
+
+        private void close_MouseLeave(object sender, EventArgs e)
+        {
+            close.Image = Resources.close_b;
+        }
+
+        private void close_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void save_MouseHover(object sender, EventArgs e)
+        {
+            save.Image = Resources.save_a;
+        }
+
+        private void save_MouseLeave(object sender, EventArgs e)
+        {
+            save.Image = Resources.save_b;
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                using (Stream s = File.Open(saveFileDialog.FileName, FileMode.CreateNew))
+                using (StreamWriter sw = new StreamWriter(s))
+                {
+                    sw.Write(outputBox.Text);
+                }
+            }
+        }
+
+        private void encryption_MouseHover(object sender, EventArgs e)
+        {
+            encryption.Image = Resources.encrypt_a;
+        }
+
+        private void encryption_MouseLeave(object sender, EventArgs e)
+        {
+            encryption.Image = Resources.encrypt_b;
+        }
+
+        private void encryption_Click(object sender, EventArgs e)
+        {
+            PasswordHash = keyBox.Text;
+            outputBox.Text = Encrypt(inputBox.Text);
+        }
+
+        private void decryption_MouseHover(object sender, EventArgs e)
+        {
+            decryption.Image = Resources.decrypt_a;
+        }
+
+        private void decryption_MouseLeave(object sender, EventArgs e)
+        {
+            decryption.Image = Resources.decrypt_b;
+        }
+
+        private void decryption_Click(object sender, EventArgs e)
+        {
+            PasswordHash = keyBox.Text;
+            outputBox.Text = Decrypt(inputBox.Text);
+        }
+    }
+}
+
